c491a4b1168b32b87d4995f4a5b9840951f0e796
cyntya vitoria <202330020463@estudante.rn.gov.br>
2025-11-13 13:41:29 +0000

    alterações

--- funcoes.js
+++ funcoes.js
@@ -3,8 +3,14 @@ function fazerlogin(){
     let senha = document.getElementById('senha').value 
 
     if(login === 'cenep' && semha === 'cenep123'){
+        alert('login ou senha correto!')
+        localStorage.setItem('nome', login)
         window.location.href = 'pagina2.html'
     }else{
         alert('login ou senha incorretos')
     }
+
 }
+ 
+document.getElementById('titulo').innerHTML +=
+ localStorage.getItem('titulo')
